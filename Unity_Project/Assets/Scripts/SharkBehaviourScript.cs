541bedfef220d747148aa225fdf5d49bf0cb2a21
Rogerio Altmeyer <raltmeyer@antares.local>
2020-03-27 18:07:09 -0300

    Initial push

---
+++ Unity_Project/Assets/Scripts/SharkBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkBehaviourScript : MonoBehaviour
+{
+    float direction_x,direction_y,rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction_x = Random.Range(-2.0f, 2.0f);
+        direction_y = Random.Range(-2.0f, 2.0f);
+        rotation = Random.Range(-1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction_x * Time.deltaTime,0f, direction_x * Time.deltaTime);
+        //transform.Rotate(rotation, rotation, rotation, Space.Self);
+        transform.Rotate(0f, rotation, 0f, Space.Self);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.name);
+
+        if (collision.gameObject.name == "RigidBodyFPSController")
+        {
+
+        }
+        direction_x = Random.Range(-10.0f, 10.0f);
+        direction_y = Random.Range(-10.0f, 10.0f);
+        rotation = Random.Range(-2.0f, 2.0f);
+
+    }
+
+}
---
+++ Unity_Project/Assets/Scripts/WallsBehaviourScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallsBehaviourScript : MonoBehaviour
+{
+    Vector3 originalPosition;
+    int direction;
+
+    void Start()
+    {
+        originalPosition = transform.position; // We store the current position
+        direction = 0;
+    }
+
+    void Update()
+    {
+        if (direction == 0)
+        {
+            if (originalPosition.y > -5F)
+            {
+                originalPosition.y -= 0.5F * Time.deltaTime;
+            } else
+            {
+                direction = 1;
+            }
+        } else {  //direction=1
+            if (originalPosition.y < 0F)
+            {
+                originalPosition.y += 0.5F * Time.deltaTime;
+            }
+            else
+            {
+                direction = 0;
+            }
+        }
+
+        transform.position = originalPosition;
+
+    }
+
+
+
+
+}
